94fd7a0af6413e7fd4a787b415db14f990564a51
AleksZelenchuk <zelenchuk.oleks@gmail.com>
2018-07-23 13:28:47 +0300

    Init

---
+++ TabsInfo/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Checkout/js/view/shipping-address/address-renderer/default':'Dv_TabsInfo/js/view/shipping-address/address-renderer/default'
+        }
+    }
+
+};
