6d016ad158c8f411fa297055046cd858567a9d90
AleksZelenchuk <zelenchuk.oleks@gmail.com>
2018-08-06 13:42:36 +0300

    Update requirejs-config.js

--- TabsInfo/view/frontend/requirejs-config.js
+++ TabsInfo/view/frontend/requirejs-config.js
@@ -1,7 +1,7 @@
 var config = {
     map: {
         '*': {
-            'Magento_Checkout/js/view/shipping-address/address-renderer/default':'Dv_TabsInfo/js/view/shipping-address/address-renderer/default'
+            'Magento_Checkout/js/view/shipping-address/address-renderer/default':'Namespace_TabsInfo/js/view/shipping-address/address-renderer/default'
         }
     }
 
